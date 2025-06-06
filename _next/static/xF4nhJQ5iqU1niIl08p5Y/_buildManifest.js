cf0b6caafdc9fb66348e636c08781b232d7b7af0
joonb14 <joonb14@gmail.com>
2025-06-06 10:43:06 +0900

    Updates

---
+++ _next/static/xF4nhJQ5iqU1niIl08p5Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/b26553aa9fa4e1ed.css","static/chunks/pages/index-47fa9724838a1fef.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
